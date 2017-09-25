a8c9cbc48bcd8d6de34dc7360052825fddd13180
fawda123 <mbafs2012@gmail.com>
2017-09-25 16:51:55 -0700

    added channel type to oedat, mmdat

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -21,13 +21,13 @@ fls <- list.files('ignore', pattern = '\\.csv$', full.names = TRUE) %>%
   )
 
 # environmental data
-envdat <- fls %>% 
+evdat <- fls %>% 
   filter(grepl('algSiteData', fl)) %>% 
   unnest %>% 
   rename(site = SampleID_old)
 
 # latlon, clip by sheds
-latlon <- envdat %>% 
+latlon <- evdat %>% 
   dplyr::select(site, New_Lat, New_Long) %>% 
   unique %>% 
   na.omit
@@ -37,8 +37,8 @@ latlon <- raster::intersect(latlon, sheds) %>%
   data.frame %>% 
   dplyr::select(site, New_Lat, New_Long)
 
-# filter envdat by latlon
-envdat <- envdat %>%  
+# filter evdat by latlon
+evdat <- evdat %>%  
   filter(site %in% latlon$site)
 
 # OE data, filter by sites in latlon
@@ -65,8 +65,55 @@ mmdat <- fls %>%
   left_join(latlon, ., by = 'site') %>% 
   na.omit
 
+##
+# get channel type
+
+# site/key lookup
+kydat <- read.csv('ignore/luStation.txt', stringsAsFactors = FALSE) %>% 
+  dplyr::select(StationID, MasterID) %>% 
+  rename(
+    sitetmp = MasterID, 
+    stationcode = StationID
+    )
+
+# rafi's channel data
+chdat <- read.csv('ignore/csci.alg.df.csv', stringsAsFactors = FALSE) %>%
+  dplyr::select(StationCode, ChannelClass_3hf) %>%
+  rename(
+    chcls = ChannelClass_3hf, 
+    stationcode = StationCode
+    ) %>%
+  left_join(kydat, by = 'stationcode') %>%
+  dplyr::select(sitetmp, chcls)
+
+# # channel data
+# chdat <- read.csv('ignore/tblChannelEngineering.txt', stringsAsFactors = FALSE) %>%
+#   dplyr::select(stationcode, channeltype) %>%
+#   rename(chcls = channeltype) %>%
+#   left_join(kydat, by = 'stationcode') %>%
+#   dplyr::select(sitetmp, chcls)
+
+# add chdat to mmdat
+mmdat <- mmdat %>% 
+  mutate(
+    sitetmp = gsub('_[0-9]+\\..*$', '', site)
+  ) %>% 
+  left_join(chdat, by = 'sitetmp') %>% 
+  dplyr::select(-sitetmp)
+
+# add chdat to oedat
+oedat <- oedat %>% 
+  mutate(
+    sitetmp = gsub('_[0-9]+\\..*$', '', site)
+  ) %>% 
+  left_join(chdat, by = 'sitetmp') %>% 
+  dplyr::select(-sitetmp)
+
+##
+# save 
+
 save(mmdat, file = 'data/mmdat.RData', compress = 'xz')
-save(envdat, file = 'data/envdat.RData', compress = 'xz')
+save(evdat, file = 'data/evdat.RData', compress = 'xz')
 save(oedat, file = 'data/oedat.RData', compress = 'xz')
 save(sheds, file = 'data/sheds.RData', compress = 'xz')
 
